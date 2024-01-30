a0381e0f32ce6f5a4d9c6f78248671dde198ef72
Eduardo Lopez <ejlopez05@outlook.com>
2024-01-30 17:22:10 -0500

    switched to quoters, added ideas and to-do list,

--- main.py
+++ main.py
@@ -2,29 +2,73 @@
 Eduardo Lopez
 01/29/24
 APCSP Create Performance Task
+Cryptogram!
 '''
+
+'''
+** IDEAS SO FAR: **
+--> Create a dictionary to encrypt each letter
+--> Function to traverse the diverse the list of the alphabet, ykyk
+--> Keep or remove having to solve for the author's name (if there is one)?
+--> For text-based program: create a "board" that updates currently known letters
+
+** TO-DO: **
+[ ] Encrypt each letter (do I need a dictionary?)
+[ ] Create a key for each letter
+[ ] Create external interface for the main game
+[ ] Random number to leave X amount of letters known (depending on length of quote?)
+'''
+
 # Imports
-from random_word import RandomWords
-from quote import quote
- 
-# Generates a random word 
-r = RandomWords()
-def generate_word():
-    random_word = r.get_random_word()
-    print("Keyword Generated: ", random_word)
-    return random_word
+from quoters import Quote
+import time
 
-# Generates a random quote based on the random word generated
-# in generate_word()
-def generate_quote(random_word):
-    random_quote = quote(random_word, limit=1)
-    # This for loop is necessary when only printing the quote as the quote function returns 
-    # a list of dictionaries with information about a quote
-    for i in range(len(random_quote)):
-        print("\nQuote Generated: ", random_quote[i]["quote"]) #Will print only the value next to the quote key.
-        quote = random_quote[i]["quote"]
-    return quote
+# Generates a random quote and appends each character to a list
+def generate_quote():
+    quote = Quote.print().upper()
+    print(quote)
+    while "HAND-PICKED RELATED QUOTES" in quote:
+        quote = Quote.print().upper()
+    quote_list = []
+    for i in range(len(quote)):
+        if quote[i] == '"' and i > 0:
+            return quote_list
+        else:
+            quote_list.append(quote[i])
+    # for i in range(len(quote_list)):
+    #     print(quote_list[i])
+    return quote_list
 
-# Main
-random_word = generate_word()
-random_quote = generate_quote(random_word)
+# Encrypts every letter found in the quote
+def encrypt_quote(quote):
+    encrypted_quote = []
+    for char in quote:
+        encrypted_quote.append(char)
+    # for i in range(len(encrypted_quote)):
+    #     print(encrypted_quote[i])
+    for i in range(len(encrypted_quote)):
+        if encrypted_quote[i].isalpha():
+            encrypted_quote[i] = "_"
+    return encrypted_quote
+
+# Updates the puzzle with correct guess
+def guess(letter, quote, encrypted_quote):
+    pass
+
+# Generates known keys for each letter
+def generate_library():
+    pass
+
+# Prints out the current state of the game
+def generate_game(encrypted_quote):
+    for char in encrypted_quote:
+        print(char, end="")
+    # print(encrypted_quote)
+
+# Does what the name says 
+def run_game():
+    quote = generate_quote()
+    encrypted_quote = encrypt_quote(quote)
+    generate_game(encrypted_quote)
+    
+run_game()
